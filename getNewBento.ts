8de45eb3689a28f68cf4f507d5b06aa3502810d7
Srikar Empati <166027817+srikarnarayanempati@users.noreply.github.com>
2025-08-07 00:35:02 +0530

    Add files via upload

--- getNewBento.ts
+++ getNewBento.ts
@@ -1,1 +1,21 @@
+const apiUrl = "https://opbento.edgexhq.tech/api/bento?n=Srikar%20Empati&g=srikarnarayanempati&x=srikarempati01&l=srikarempati&i=&p=https%3A%2F%2Fsrikarempati.vercel.app%2F&z=0e416";
+interface BentoResponse {
+  url: string;
+}
 
+const fetchBentoUrl = async (apiUrl: string): Promise<string> => {
+  try {
+    const response = await fetch(apiUrl);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data: BentoResponse = (await response.json()) as BentoResponse;
+    return data.url;
+  } catch (error) {
+    console.error("Error fetching Bento URL:", error);
+    throw error;
+  }
+};
+
+// @ts-ignore
+fetchBentoUrl(apiUrl);
