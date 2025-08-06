d46398ded36f059bde584a3589343639a7ceb521
Srikar Empati <166027817+srikarnarayanempati@users.noreply.github.com>
2025-08-07 00:45:50 +0530

    Add files via upload

--- getNewBento.ts
+++ getNewBento.ts
@@ -1,4 +1,4 @@
-const apiUrl = "https://opbento.edgexhq.tech/api/bento?n=Srikar%20Empati&g=srikarnarayanempati&x=srikarempati01&l=srikarempati&i=&p=https%3A%2F%2Fsrikarempati.vercel.app%2F&z=0e416";
+const apiUrl = "https://opbento.edgexhq.tech/api/bento?n=Srikar%20Empati&g=srikarnarayanempati&x=srikarempati01&l=srikarempati&i=https%3A%2F%2Fi.postimg.cc%2FHnRrXBmw%2Fsrikar.jpg&p=https%3A%2F%2Fsrikarempati.vercel.app%2F&z=0e416";
 interface BentoResponse {
   url: string;
 }
